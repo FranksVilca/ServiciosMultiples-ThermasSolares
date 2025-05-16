709600f190d41259e47060100c30442175fdeb2e
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-05-15 23:30:13 -0500

    feat(tailwind): add file tailwind.conf for dark theme

---
+++ src/Components/Home/HeroSections.tsx
@@ -0,0 +1,36 @@
+const Service = {
+    title:"Nuestros Servicios",
+    Servicios:[
+      {subtitulo:"Venta de thermas solares"},
+      {subtitulo:"Manteniento de Thermas"},
+      {subtitulo:"Limpieza de pozos"}
+    ],
+
+}
+
+const HeroSections: React.FC = () => {
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="flex-1">
+          <div className="">Su amigo tecnico preocupandose por usted</div>
+          <div>Ofrecemos servicios de plomería confiables que cubren todas sus necesidades. Desde reparaciones sencillas de tuberías hasta instalaciones complejas.</div>
+          <div>{Service.title}</div>
+          <div></div>
+        </div>
+        <div className="flex-1">
+          <div className="bg-theme w-full">asd</div>
+          <div className="bg-[#939393] w-full">asd</div>
+        </div>
+        <div className="flex-1 justify-items-center">
+          <button className="bg-[#5BB543] rounded-full w-20 h-20">as</button>
+          <div className="text-[#2C2E47] w-32">
+            Puedes Observar nuestro trabajos
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSections;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
 import Footer from "@/Components/footer";
 import Header from "@/Components/header";
 import Headerh from "@/Components/headerh";
+import HeroSections from "@/Components/Home/HeroSections";
 
 export default function Home() {
   return (
     <div>
       <Headerh />
       <Header />
+      <HeroSections />
+      
       <Footer />
     </div>
   );
