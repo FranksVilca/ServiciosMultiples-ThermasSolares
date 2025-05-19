65185542ab38fb078fe07d7c61c19467f213ed9d
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-05-19 15:26:06 -0500

    feat(Servicios):terminated squelate

---
+++ src/Components/Servicios/servicePage.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Services = {
+  servicios: [
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+    {
+      title: "Servicio",
+      text: "asdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsaasdasdasdadsa",
+      image: "",
+    },
+  ],
+};
+
+const servicePage: React.FC = () => {
+  return (
+    <div>
+      <div className="mb-40">
+        <div className="w-full bg-third h-96"></div>
+        <div className="my-8 mx-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {Services.servicios.map((item, index) => (
+            <div key={index} className="gap-4 rounded-2xl">
+              <div className="bg-third w-full h-40 rounded-t-2xl">
+                {item.image}
+              </div>
+              <div className="bg-[#000] text-[#5BB543] w-1/3 rounded-3xl text-center my-1 py-2 text-xl font-serif">
+                {item.title}
+              </div>
+              <div className="flex justify-center my-2">{item.text.slice(0, 50)}...</div>
+              <div className="bg-second rounded-3xl text-center my-1 py-2 w-1/3 font-serif font-semibold mx-auto">
+                Leer Mas
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default servicePage;
---
+++ src/app/Servicios/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/Components/footer";
+import Header from "@/Components/header";
+import ServicePage from "@/Components/Servicios/servicePage";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <ServicePage />
+      <Footer />
+    </div>
+  );
+}
