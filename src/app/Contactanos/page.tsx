7b87eb241d61203df3f755127000d9b1baefede2
FranksVilca <franksjaviervilcaquispe@gmail.com>
2025-08-14 11:11:55 -0500

    Repair:Delete unused component

--- src/app/Contactanos/page.tsx
+++ src/app/Contactanos/page.tsx
@@ -1,7 +1,6 @@
 import ContactanosPage from "@/Components/Contactanos/ContactanosPage";
 import Footer from "@/Components/footer";
 import Header from "@/Components/header";
-import ServicePage from "@/Components/Servicios/servicePage";
 
 export default function Home() {
   return (
